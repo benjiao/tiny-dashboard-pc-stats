bd6d0d12e3a9e736e64e0ae8b04cad3ef6949dd6
“Benjie <hi@benjie.me>
2016-10-23 17:59:31 +0800

    Updated install config

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="tiny-dashboard-pc-stats",
     version="0.1",
     author="Benjie Jiao",
     author_email="hi@benjie.me",
-    description=("Send PC stats to an Arduino dashboard via "),
-    test_suite='tests',
-    entry_points={
-        'console_scripts': [
-            'pc-stats=pc_stats.cli:cli'
-        ]
-    },
+    description=("Send PC stats to an Arduino dashboard via USB"),
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'Click', 'pyserial', 'psutil'
     ],
+    entry_points={
+        'console_scripts': [
+            'pc-stats=pc_stats.pc_stats:cli'
+        ]
+    }
 )
